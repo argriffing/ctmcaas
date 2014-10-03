afa35c3a8570f6de54865001526bd5019926a399
alex <argriffi@ncsu.edu>
2014-10-03 18:37:08 -0400

    instead of adding more wrappers for the simpler log likelihood interface, begin defining a more flexible interface and writing a new implementation

---
+++ adv-log-likelihoods/README.md
@@ -0,0 +1,1 @@
+This is a more advanced interface for log likelihoods calculations.
---
+++ adv-log-likelihoods/ll.py
@@ -0,0 +1,9 @@
+"""
+Implement log likelihoods for complicated models.
+
+This interface takes some care about memory usage,
+while allowing more subtlety in the representation of observed data,
+and while allowing more flexibility in the representation of
+inhomogeneity of the process across branches.
+
+"""
